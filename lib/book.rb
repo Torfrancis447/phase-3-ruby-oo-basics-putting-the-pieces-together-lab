7c12c0a8e39a20a50ceb36c2c0c45a563e4a2643
tor <torfrancis447@gmail.com>
2022-12-13 19:58:43 -0500

    commit tree 6fb592fba254bbc4411ca01068b9de5c047ec2b3

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,24 @@
+require "pry"
+class Book
+    attr_accessor :title, :author, :page_count, :genre
+    def initialize(title="book title")
+        @title = title
+        
+    end
+    def author= (author)
+        @author = author
+    end
+    def page_count= (page_count)
+        @page_count = page_count
+    end
+    def genre= (genre)
+        @genre = genre
+    end
+    def turn_page
+        puts "Flipping the page...wow, you read fast!"
+    end
+end
 
+book = Book.new("Ender's Game")
+# binding.pry
 
